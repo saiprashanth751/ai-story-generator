ace06aea5945361cc661b4c697709098c0fcdf35
“saiprashanth” <saiprashanth751@gmail.com>
2025-04-05 00:53:45 +0530

    removed-static-files

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,10 @@ app.use(express.json());
 app.use('/api/story', storyRoutes);
 
 // Serve React frontend
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, '../frontend/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+// });
 
 // Start server
 const PORT = process.env.PORT || 5000;
